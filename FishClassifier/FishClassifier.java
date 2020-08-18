6e02145fd28875ac9dcc00b06051f2de0b944ab3
afiqahshamin <67795457+afiqahshamin@users.noreply.github.com>
2020-08-18 15:39:55 +0800

    Update FishClassifier.java

--- FishClassifier/FishClassifier.java
+++ FishClassifier/FishClassifier.java
@@ -6,7 +6,9 @@ import org.datavec.api.split.FileSplit;
 import org.datavec.api.split.InputSplit;
 import org.datavec.image.loader.BaseImageLoader;
 import org.datavec.image.recordreader.ImageRecordReader;
+import org.datavec.image.transform.ImageTransform;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.GradientNormalization;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.inputs.InputType;
@@ -34,12 +36,37 @@ import java.util.Random;
 public class FishClassifier {
     public static Logger log = LoggerFactory.getLogger(FishClassifier.class);
     private static int seed = 1234;
-    private static int batchSize = 100;
+
     private static int labelIndex = 1;
     private static int numPossibleLabels = 3;
+    private static int channels = 3;
+    private static int height = 224;
+    private static int width = 224;
+
+    //images are of format given by allowedExtension
+    private static final String [] allowedExtensions = BaseImageLoader.ALLOWED_FORMATS;
+
+    //random number generator (with seed for reproducibility)
+    //private static final Random rng = new Random(123);
+
+    private static String dataDirectory;
+    private static String downloadLink;
+
+    private static ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+    private static InputSplit trainData,testData;
+    private static int batchSize = 50;
+
+    //scale input to 0-1
+    private static DataNormalization scaler = new ImagePreProcessingScaler(0,1);
+    private static ImageTransform transform;
+
 
     public static void main(String[] args) throws Exception{
+
+        //logging process of loading data
         log.info("Loading data");
+
+        //getting home path for fish dataset
         String homePath = System.getProperty("user.home");
         Path dataPath = Paths.get(homePath,".deeplearning4j","data","Fish Species Datasets");
         System.out.println(dataPath);
@@ -62,8 +89,8 @@ public class FishClassifier {
         InputSplit testData = trainTestSplit[1];
         System.out.println(trainData);
 
-        ImageRecordReader trainRecordReader = new ImageRecordReader(224, 224, 3, labelGenerator);
-        ImageRecordReader testRecordReader = new ImageRecordReader(224,224, 3, labelGenerator);
+        ImageRecordReader trainRecordReader = new ImageRecordReader(height, width, channels, labelMaker);
+        ImageRecordReader testRecordReader = new ImageRecordReader(height,width, channels, labelMaker);
 
         trainRecordReader.initialize(trainData);
         testRecordReader.initialize(testData);
@@ -75,51 +102,53 @@ public class FishClassifier {
         testIter.setPreProcessor(scaler);
 
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-            .seed(1234)
-            .weightInit(WeightInit.XAVIER)
-            .updater(new Adam(0.01))
-            .l2(1e-3)
-            .list()
-            .layer(0, new ConvolutionLayer.Builder(new int[]{3,3}, new int[]{4,4})
-                .name("conv1")
-                .nIn(3)
-                .nOut(96)
+                .seed(seed)
+                .weightInit(WeightInit.XAVIER)
                 .activation(Activation.RELU)
-                .build())
-            .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                .name("maxpool1")
-                .kernelSize(3,3)
-                .stride(2,2)
-                .padding(1,1)
-                .build())
-            .layer(2, new ConvolutionLayer.Builder(new int[] {5,5}, new int[] {1,1}, new int[] {2,2})
-                .name("conv2")
-                .nOut(128)
-                .activation(Activation.RELU)
-                .build())
-            .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                .name("maxpool2")
-                .kernelSize(3,3)
-                .stride(2,2)
-                .build())
-            .layer(4, new DenseLayer.Builder()
-                .name("ffn1")
-                .nOut(128)
-                .activation(Activation.RELU)
-                .dropOut(0.2)
-                .build())
-            .layer(5, new DenseLayer.Builder()
-                .name("ffn2")
-                .nOut(128)
-                .dropOut(0.2)
-                .build())
-            .layer(6, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                .name("output")
-                .nOut(3)
-                .activation(Activation.SOFTMAX)
-                .build())
-            .setInputType(InputType.convolutional(224,224,3))
-            .build();
+                .updater(new Adam(0.001))
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing/exploding gradient
+                .l2(5 * 1e-4)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(new int[]{11,11}, new int[]{4,4})
+                        .name("conv1")
+                        .nIn(channels)
+                        .nOut(96)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .name("maxpool1")
+                        .kernelSize(3,3)
+                        .stride(2,2)
+                        .padding(1,1)
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(new int[] {5,5}, new int[] {1,1}, new int[] {2,2})
+                        .name("conv2")
+                        .nOut(128)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .name("maxpool2")
+                        .kernelSize(3,3)
+                        .stride(2,2)
+                        .build())
+                .layer(4, new DenseLayer.Builder()
+                        .name("ffn1")
+                        .nOut(128)
+                        .activation(Activation.RELU)
+                        .dropOut(0.2)
+                        .build())
+                .layer(5, new DenseLayer.Builder()
+                        .name("ffn2")
+                        .nOut(128)
+                        .dropOut(0.2)
+                        .build())
+                .layer(6, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .name("output")
+                        .nOut(3)
+                        .activation(Activation.SOFTMAX)
+                        .build())
+                .setInputType(InputType.convolutional(224,224,3))
+                .build();
 
         MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
@@ -135,11 +164,11 @@ public class FishClassifier {
 //            new ScoreIterationListener(5)
 //        );
 
-        model.fit(trainIter, 5);
+        model.fit(trainIter, 40);
         Evaluation eval = model.evaluate(testIter);
         log.info(eval.stats());
 
-
+0
     }
 }
 
